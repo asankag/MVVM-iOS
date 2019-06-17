09cbd6900b06186970776a9abcf2473232ebf4b4
Asanka <gankewala@gmail.com>
2019-06-17 16:59:17 +0530

    Initial Commit

---
+++ testInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testInterview
+//
+//  Created by Asanka on 6/17/19.
+//  Copyright © 2019 Asanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
